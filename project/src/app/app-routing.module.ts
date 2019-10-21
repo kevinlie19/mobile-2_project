b9cc8923c11c0083e6e9c5d19a139d14b9c97903
Kevin Lie <Me@Kevin.local>
2019-10-21 12:49:15 +0700

    Add Welcome/Splash Page

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+  {path: 'welcome', loadChildren: './welcome/welcome.module#WelcomePageModule'},
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./home/home.module').then((m) => m.HomePageModule),
+  },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules}),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
