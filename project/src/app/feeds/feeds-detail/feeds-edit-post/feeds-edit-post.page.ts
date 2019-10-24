974705962963e8bd607f68f6593a4b533f21dd6c
PeterGautama <42568890+PeterGautama@users.noreply.github.com>
2019-10-24 19:56:49 +0700

    wip-feeds

---
+++ project/src/app/feeds/feeds-detail/feeds-edit-post/feeds-edit-post.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feeds-edit-post',
+  templateUrl: './feeds-edit-post.page.html',
+  styleUrls: ['./feeds-edit-post.page.scss'],
+})
+export class FeedsEditPostPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
