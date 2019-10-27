1d38b1f2cf3c7796aea283e59b9d960654d76d98
Antonius Filian <antoniusfilian@gmail.com>
2019-10-27 17:22:44 +0700

    notification update

--- project/src/app/feeds-add-post/feeds-add-post.page.ts
+++ project/src/app/feeds-add-post/feeds-add-post.page.ts
@@ -1,5 +1,7 @@
+import { Router } from '@angular/router';
 import {Component, OnInit} from '@angular/core';
-import {NavController} from '@ionic/angular';
+import {NavController, Platform} from '@ionic/angular';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 
 @Component({
   selector: 'app-feeds-add-post',
@@ -7,11 +9,32 @@ import {NavController} from '@ionic/angular';
   styleUrls: ['./feeds-add-post.page.scss'],
 })
 export class FeedsAddPostPage implements OnInit {
-  constructor(private navCtrl: NavController) {}
+  constructor(
+    private plt: Platform,
+    private navCtrl: NavController,
+    private localNotifications: LocalNotifications,
+    private router: Router) {
+    this.localNotifications.on('click').subscribe(notification => {
+      this.router.navigateByUrl('/request');
+    });
+  }
 
   ngOnInit() {}
 
   onClickClose() {
     this.navCtrl.setDirection('back');
+  }
+
+  addPost(){
+    this.localNotifications.schedule({
+      text: 'Hi! You you have a new request!',
+      trigger: { at: new Date(new Date().getTime() + 5000) },
+      led: 'FF0000',
+      sound:  'file://sound.mp3',
+      smallIcon: '../../assets/images/LogoCibo.png',
+      icon: '../../assets/images/LogoCibo.png',
+      vibrate: true,
+    });
+    this.router.navigateByUrl('/feeds');
   }
 }
