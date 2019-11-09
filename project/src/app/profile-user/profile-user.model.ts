7781de632cb64b9f14dc65c5d2d47016f8e4f0e3
PeterGautama <42568890+PeterGautama@users.noreply.github.com>
2019-11-09 11:09:06 +0700

    connect profile-user

--- project/src/app/profile-user/profile-user.model.ts
+++ project/src/app/profile-user/profile-user.model.ts
@@ -1,14 +1,31 @@
-export type Profile = {
-  id: string;
-  item_name: string;
-  status: string;
-  category: string;
-  description: string;
-  buyDate: string;
-  expDate: string;
-  timestamps: string;
-  itemImageUrl: string;
+export type UserProfile = {
+  user: userDetail;
+  post: Array<PostObject>;
+};
+
+export type userDetail = {
+  id: number;
+  email: string;
   username: string;
+  full_name: string;
+  password: string;
+  phone_number: string;
   location: string;
   avatar: string;
+  gender: string;
+  following: [];
+  follower: [];
+};
+
+export type PostObject = {
+  id: number;
+  user_id: number;
+  item_name: string;
+  image: string;
+  buy_date: string;
+  exp_date: string;
+  category: string;
+  description: string;
+  tag: 'Available' | 'Unavailable' | 'Expired';
+  timestamp: string;
 };
