978a8aff9fe879505478e92da5e104a121125512
PeterGautama <42568890+PeterGautama@users.noreply.github.com>
2019-11-20 14:45:02 +0700

    Fixing Search Scene

--- project/src/app/search/search.page.ts
+++ project/src/app/search/search.page.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
-import { Storage } from "@ionic/storage";
+import {Storage} from '@ionic/storage';
 
-import { Search } from "./search.model";
-import { APISetting } from "../constant/API";
+import {Search} from './search.model';
+import {APISetting} from '../constant/API';
 
 @Component({
-  selector: "app-search",
-  templateUrl: "./search.page.html",
-  styleUrls: ["./search.page.scss"]
+  selector: 'app-search',
+  templateUrl: './search.page.html',
+  styleUrls: ['./search.page.scss'],
 })
 export class SearchPage implements OnInit {
   constructor(private router: Router, private storage: Storage) {}
@@ -18,30 +18,52 @@ export class SearchPage implements OnInit {
   isInputEntered: boolean;
   isLoading: boolean;
 
-  ngOnInit() {
+  async ngOnInit() {
     this.isInputEntered = false;
+    this.isLoading = true;
+    let userToken = await this.storage.get('userToken');
+    let response = await fetch(
+      APISetting.API_ENDPOINT + 'feature/search/?query=',
+      {
+        mode: 'cors',
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          Authorization: userToken,
+        },
+      },
+    );
+
+    const result = await response.json();
+    this.loadedSearch = result.data;
+    this.isLoading = false;
+  }
+
+  ionViewWillEnter() {
+    this.ngOnInit();
   }
 
   async onInputSearch(keyword: string) {
     this.isLoading = true;
-    let userToken = await this.storage.get("userToken");
+    let userToken = await this.storage.get('userToken');
     let response = await fetch(
-      APISetting.API_ENDPOINT + "feature/search/?query=" + keyword,
+      APISetting.API_ENDPOINT + 'feature/search/?query=' + keyword,
       {
-        mode: "cors",
-        method: "GET",
+        mode: 'cors',
+        method: 'GET',
         headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          Authorization: userToken
-        }
-      }
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          Authorization: userToken,
+        },
+      },
     );
 
     const result = await response.json();
-    this.isLoading = false;
     this.loadedSearch = result.data;
-    if (keyword === "") {
+    this.isLoading = false;
+    if (keyword === '') {
       this.isInputEntered = false;
     } else {
       this.isInputEntered = true;
@@ -49,18 +71,18 @@ export class SearchPage implements OnInit {
   }
 
   onClickAdd() {
-    this.router.navigateByUrl("/add-item");
+    this.router.navigateByUrl('/add-item');
   }
 
   onClickRequest() {
-    this.router.navigateByUrl("/request");
+    this.router.navigateByUrl('/request');
   }
 
   onClickProfile() {
-    this.router.navigateByUrl("/profile");
+    this.router.navigateByUrl('/profile');
   }
 
   onClickHome() {
-    this.router.navigateByUrl("/feeds");
+    this.router.navigateByUrl('/feeds');
   }
 }
